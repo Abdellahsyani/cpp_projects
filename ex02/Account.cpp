0a192df8fb35e5d3004f586aa3b46be8642dfe87
Abdellahsyani <syaniabdos@gmail.com>
2025-09-19 19:59:14 +0100

    try to implement bank system

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,8 +1,21 @@
 #include "Account.hpp"
 
+t User;
 
-Account::t acount;
+Account::Account(int deposit, int index, int amount, int nbDeposits, int nbWithrawals) 
+	makeDeposit(deposit);
+	_accountIndex = index;
+	_amount = amount;
+	_nbDeposits = nbDeposits;
+	_nbWithrawals = nbWithrawals;
+};
 
-Account::Account(int deposit) {};
+t::makeDeposit(int deposit)
+{
+	Account::_nbAccounts
+}
 
-
+int Account::_nbAccounts = 0;
+int Account::_totAlAmount = 0;
+int Account::_totAlNbDeposits = 0;
+int Account::_totAlNbWithrAwAls = 0;
