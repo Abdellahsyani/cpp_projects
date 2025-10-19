265cb4123d336df3f0aae4bf752e066246e9008a
Abdellahsyani <syaniabdos@gmail.com>
2025-10-19 11:54:36 +0100

    just add

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -11,14 +11,32 @@
 /* ************************************************************************** */
 
 #include <iostream>
+#include <vector>
 
+
+void get_values(std::vector<std::string> args)
+{
+	std::string filename;
+	std::string str1;
+	std::string str2;
+
+	filename = args[1];
+	std::cout << filename << std::endl;
+	str1 = args[2];
+	std::cout << str1 << std::endl;
+	str2 = args[3];
+	std::cout << str2 << std::endl;
+}
 
 int main(int ac, char **av)
 {
-	if (ac > 3)
-		do some thing:
+	std::vector<std::string> args(av, av + ac);
 
-
-
-
+	if (args.size() > 4 || args.size() < 3)
+	{
+		std::cout << "Error" << std::endl;
+		return 0;
+	}
+	get_values(args);
+	return 0;
 }
