63fd9eadac170130cce3099f6b6cdfd3d80458bf
Abdellahsyani <syaniabdos@gmail.com>
2025-10-25 17:07:16 +0100

    still add

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -12,27 +12,58 @@
 
 #include <iostream>
 #include <vector>
+#include <fstream>
 
+
+void filter_line(std::string file, std::string line, std::string str1, std::string str2)
+{
+	std::string filename = file + ".replace";
+	std::ofstream file(filename, std::ios::app);
+	if (!filename.is_open())
+	{
+		std::cout << "Error: can't open this file" << std::endl;
+		return;
+	}
+	size_t pos = 0;
+	size_t found;
+	std::string new_line;
+	while ((found = line.find(str1, pos)) != std::string::npos)
+	{
+		new_line += line.substr(pos, found - pos);
+		new_line += str2;
+		found += str1.length();
+	}
+	new_line += line.substr(pos);
+	std::cout << new_line << std::endl;
+}
 
 void get_values(std::vector<std::string> args)
 {
 	std::string filename;
 	std::string str1;
 	std::string str2;
+	std::ifstream file;
+	std::string line;
 
 	filename = args[1];
-	std::cout << filename << std::endl;
 	str1 = args[2];
-	std::cout << str1 << std::endl;
 	str2 = args[3];
-	std::cout << str2 << std::endl;
+	file.open(filename);
+	if (!file.is_open())
+	{
+		std::cerr << "File failed to open" << std::endl;
+		return;
+	}
+	
+	while (std::getline(file, line))
+		filter_line(filename, line, str1, str2);
 }
 
 int main(int ac, char **av)
 {
 	std::vector<std::string> args(av, av + ac);
 
-	if (args.size() > 4 || args.size() < 3)
+	if (args.size() > 4 || args.size() <= 3)
 	{
 		std::cout << "Error" << std::endl;
 		return 0;
