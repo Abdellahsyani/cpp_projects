a93e507c0bc7be5288767d6bd95cd15ff46f7700
Abdellahsyani <syaniabdos@gmail.com>
2025-11-02 13:25:54 +0100

    finsihed

--- CPP01/ex06/Harl.cpp
+++ CPP01/ex06/Harl.cpp
@@ -13,35 +13,32 @@
 #include "Harl.hpp"
 
 void Harl::debug( void ) {
-		std::cout << "[ DEBUG ] I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+		std::cout << "[ DEBUG ]" << std::endl << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
 };
 
 void Harl::info( void ) {
-		   std::cout << "[ INFO ] I cannot believe adding extra bacon costs more money. You didn’t put \
+		   std::cout << "[ INFO ]" << std::endl << "I cannot believe adding extra bacon costs more money. You didn’t put \
 enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 };
 
 void Harl::warning( void ) {
-	std::cout << "[ WARNING ] I think I deserve to have some extra bacon for free. I’ve been coming for \
+	std::cout << "[ WARNING ]" << std::endl << "I think I deserve to have some extra bacon for free. I’ve been coming for \
 years, whereas you started working here just last month." << std::endl;
 }
 
 void Harl::error( void ) {
-		std::cout << "[ ERROR ] This is unacceptable! I want to speak to the manager now." << std::endl;
+		std::cout << "[ ERROR ]" << std::endl << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
 void Harl::complain(std::string level ) {
 
 	int n = -1;
-		void (Harl::*ptr[])() = { &Harl::debug, &Harl::info, &Harl::warning, &Harl::error };
 	std::string levels[] = { "DEBUG", "INFO", "WARNING", "ERROR"};
 
 	for (int i = 0; i < 4; i++)
 	{
 		if (level == levels[i])
-		{
 			n = i;
-		}
 	}
 	switch (n)
 	{
@@ -53,8 +50,9 @@ void Harl::complain(std::string level ) {
 			this->warning();
 		case 3:
 			this->error();
+			break;
 		default:
-			std::cout << "Unknown command" << std::endl;
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 		break;
 	}
 }
