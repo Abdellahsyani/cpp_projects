51035c4202fdf1f9e552b9ca178d93ca79bb8542
Abdellahsyani <syaniabdos@gmail.com>
2025-11-23 14:03:15 +0100

    still coding

--- CPP03/ex00/ClapTrap.cpp
+++ CPP03/ex00/ClapTrap.cpp
@@ -12,28 +12,66 @@
 
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap() {};
+/**
+ * Default Constructor: This called when an object created
+ *  - does not take any parameter
+ */
+ClapTrap::ClapTrap() { std::cout << "Default constructor called" << std::endl; };
 
+
+/**
+ * Parametrize Constructor: This one called when the object is created
+ *  - initiliaze all class memeber by using list initializer
+ *  	before the buddy constructor
+ */
 ClapTrap::ClapTrap(std::string Name, int HitPoints, int	EnergyPoints, int AttackDamage) :
-	Name(name), HitPoints(10), EnergyPoints(10), AttackDamage(0) {};
+	Name(name), HitPoints(10), EnergyPoints(10), AttackDamage(0) { std::cout << "Parametrize Constructor called" << std::endl; };
 
+/**
+ * Copy Constructor: This one copy all members from an object to another one that created in the same time
+ *  - Heppens only when the object is bieng created
+ *  	- behavior: b,  a(b)
+ */
+ClapTrap::ClapTrap(const ClapTrap& other) {
+	this->Name = other.Name;
+	this->HitPoints = other.HitPoints;
+	this->EnergyPoints = other.EnergyPoints;
+	this->AttackDamage = other.AttackDamage;
+}
+
+/**
+ * Copy assignment Constructor: This type of constructor create an object from an existing one
+ *  - so it behaves like this: a, b, a = b
+ */
 ClapTrap& ClapTrap::operator=(const ClapTrap& other) {
 	this->Name = other.Name;
 	this->HitPoints = other.HitPoints;
 	this->EnergyPoints = other.EnergyPoints;
 	this->AttackDamage = other.AttackDamage;
 
-	return *this;
+	return (*this);
 }
 
+/**
+ * SetName: this is setter of attribute name
+ *  - name: The name to be setted
+*/
 void ClapTrap::SetName(const std::string name) {
 	this->name = name;
 }
 
+/**
+ * GetName: This is the getter to get the name
+ * - name: the name to be getted from the class
+ */
 std::string ClapTrap::GetName() const {
 	return this->name;
 }
 
+/**
+ * attack: a method to attack with
+ *  - target: the target that will be attecked
+ */
 void ClapTrap::attack(const std::string& target) {
 	//start method
 	if (this->hitpoints == 0  || this->EnergyPoints == 0)
