dbbf177c6d53940e0fb5dbbc60e91588641a0735
Abdellahsyani <syaniabdos@gmail.com>
2025-11-28 18:50:59 +0100

    implement inheritance class

--- CPP03/ex01/ScavTrap.cpp
+++ CPP03/ex01/ScavTrap.cpp
@@ -6,10 +6,24 @@
 /*   By: asyani <asyani@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 18:23:47 by asyani            #+#    #+#             */
-/*   Updated: 2025/11/28 18:35:12 by asyani           ###   ########.fr       */
+/*   Updated: 2025/11/28 18:50:27 by asyani           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
 
-ScavTrap::ScavTrap() { std::cout << "Constructor called" << std::endl; };
+ScavTrap::ScavTrap() : ClapTrap("")
+{
+	this->HitPoints = 100;
+	this->EnergyPoints = 50;
+	this->AttackDamage = 20;
+	std::cout << "Default Constructor called" << std::endl;
+}
+
+ScavTrap::ScavTrap(std::string name) : ClapTrap(name)
+{
+	this->HitPoints = 100;
+	this->EnergyPoints = 50;
+	this->AttackDamage = 20;
+	std::cout << "ScavTrap Parametrize Constructor called" << std::endl;
+}
