69ea13b65e4e6149ddd6a375bf6569f8e55faf9d
Abdellahsyani <syaniabdos@gmail.com>
2025-12-06 17:23:55 +0100

    add fragtrap

---
+++ CPP03/ex02/FragTrap.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asyani <asyani@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/06 17:08:52 by asyani            #+#    #+#             */
+/*   Updated: 2025/12/06 17:23:34 by asyani           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <ClapTrap.hpp>
+
+
+class FragTrap : public ClapTrap {
+	public:
+		FragTrap();
+		FragTrap(std::string name);
+		FragTrap(const FragTrap& other);
+		~FragTrap();
+		FragTrap& operator=(const FragTrap& other);
+
+		void attack(const std::string& target);
+		void guardGate();
+}
