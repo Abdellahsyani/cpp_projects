c52ffa2b9170e7ac787daffb50c83b01a8f7dff3
Abdellahsyani <syaniabdos@gmail.com>
2025-12-10 19:29:39 +0100

    modify diamonTRap

--- CPP03/ex03/DiamondTrap.cpp
+++ CPP03/ex03/DiamondTrap.cpp
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 
-#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 #include "FragTrap.hpp"
 #include "DiamondTrap.hpp"
 
