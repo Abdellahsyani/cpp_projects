228c7b7efc3ee3e6da2e03e004ae319a444072c9
Abdellahsyani <syaniabdos@gmail.com>
2026-01-14 15:53:47 +0100

    done form this one

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -5,7 +5,14 @@
 
 
 /**
- * iter: */
+ * iter: Iterates through an array and applies a function to each element.
+ *   * This is a higher-order function template that decouples array traversal 
+ *     from the logic applied to the data.
+ *
+ * @param arr:  Pointer to the first element of the array.
+ * @param len:  The number of elements to process (size_t for architectural safety).
+ * @param func: A callable entity (function, functor) to apply to each element.
+ **/
 template <typename Array, typename F>
 void iter(Array *arr, size_t const len, F func) {
   for (size_t i = 0; i < len; i++) {
--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -2,19 +2,29 @@
 #include <iostream>
 
 
-/***/
+/**
+ * func: This function accept a const template data typename
+ **/
 template <typename T>
 void func(T const& n)
 {
   std::cout << n << std::endl;
 }
 
+/**
+ * func1: This function accept a non const template data typename
+ **/
+template <typename T>
+void func1(T& n)
+{
+  n++;
+  std::cout << n << std::endl;
+}
+
 int main()
 {
-  float arr[] = {1.2f, 54.6f,  34.5f};
-  size_t const len = 3;
+  float const arr[] = {1.2f, 54.6f,  34.5f};
   
-  iter(arr, len, func<float>);
-
+  iter(arr, 3, func<float>);
   return 0;
 }
