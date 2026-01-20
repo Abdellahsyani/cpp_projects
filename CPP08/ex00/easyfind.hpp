da1201e9ea713f06cd8b6c8ff6fb67a16d67adbd
Abdellahsyani <syaniabdos@gmail.com>
2026-01-20 20:41:36 +0100

    add some lines of code

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -1,9 +1,15 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
-template <typename T>
-void easyfind(T& container, int n) {
+#include <stdexcept>
+#include <algorithm>
 
+template <typename T>
+T easyfind(T& container, int n) {
+  typename T::iterator it = find(container.begin(), container.end(), n);
+  if (it == container.end())
+    throw std::runtime_error("Value not found");
+  return it;
 }
 
 #endif // !EASYFIND_HPP
