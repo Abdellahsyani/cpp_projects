0b17033e7432509ae90c6f6346b1f1be0e20bb49
Abdellahsyani <syaniabdos@gmail.com>
2026-01-22 15:49:36 +0100

    stack done

---
+++ CPP08/ex02/MutantStack.hpp
@@ -0,0 +1,30 @@
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+#include <algorithm>
+#include <stack>
+#include <iostream>
+
+template <typename T>
+class MutantStack : public std::stack<T> {
+public:
+  MutantStack() {};
+  MutantStack(const MutantStack<T>& other) : std::stack<T>(other) {};
+  MutantStack<T>& operator=(const MutantStack<T>& other) {
+    if (this != &other)
+      std::stack<T>::operator=(other);
+    return *this;
+  };
+  virtual ~MutantStack() {};
+
+  typedef typename std::stack<T>::container_type::iterator iterator; 
+  iterator begin() {
+    return this->c.begin();
+  }
+  
+  iterator end() {
+    return this->c.end();
+  }
+};
+
+#endif // MutantStack
