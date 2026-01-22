6a274972e1cac0a990263109dc9f32d2ba6fcd25
Abdellahsyani <syaniabdos@gmail.com>
2026-01-22 15:10:38 +0100

    stack and deque

--- CPP08/ex02/MutantStack.cpp
+++ CPP08/ex02/MutantStack.cpp
@@ -3,11 +3,17 @@
 
 template <typename T>
 class MutantStack : std::stack<T> {
+  private:
+    std::stack<T> st;
 public:
   MutantStack();
   MutantStack(const MutantStack<T>& other);
   MutantStack<T>& operator=(const MutantStack<T>& other);
   ~MutantStack();
+
+  typedef typename 
 };
+
+#include "MutantStack.tpp";
 
 #endif // MutantStack
