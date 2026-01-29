db7584f53614de9ca149c6dc7025579c2c5fbb13
Abdellahsyani <syaniabdos@gmail.com>
2026-01-29 12:58:36 +0100

    start pmergme

---
+++ CPP09/ex02/PmergeMe.hpp
@@ -0,0 +1,11 @@
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <climits>
+
+
+#endif // !PMERGEME_HPP
---
+++ CPP09/ex02/main.cpp
@@ -0,0 +1,50 @@
+#include "PmergeMe.hpp"
+
+
+int StringToInt(const char *str) {
+  char *endPtr;
+  long val = std::strtol(str, &endPtr, 10);
+
+  if (*endPtr != '\0') {
+    throw std::runtime_error("Invalid Character");
+  }
+
+  if (val > INT_MAX || val < 0) {
+    throw std::runtime_error("Invalid Number");
+  }
+
+  return static_cast<int>(val);
+}
+
+
+int main(int ac, char *av[]) {
+
+  if (ac < 2) {
+    std::cout << "Error: Enter some number" << std::endl;
+    return 1;
+  }
+
+  std::vector<std::pair<int, int>> pairs;
+  std::vector<int> input;
+  try {
+    int n = 0;
+    for (int i = 1; i < ac; i++) {
+      input[n] = StringToInt(av[i]);
+      n++;
+    }
+    for (int i = 0; i < input.size(); i += 2) {
+      if (input[i] < input[i+1]) {
+        pairs.push_back(std::make_pair(input[i+1], input[i]));
+      } else {
+        pairs.push_back(std::make_pair(input[i], input[i+1]))
+      }
+    }
+
+  } catch (std::exception &e) {
+    std::cout << "Error: " << e.what() << std::endl;
+  }
+  // VectorTest();
+  // DequeTest();
+
+  return 0;
+}
