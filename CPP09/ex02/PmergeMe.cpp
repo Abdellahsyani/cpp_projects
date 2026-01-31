45698eed4e5eb3aa980cae51d3893e4600d96470
Abdellahsyani <syaniabdos@gmail.com>
2026-01-31 10:16:52 +0100

    ford-johnson algorithm still loading .......

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -4,9 +4,15 @@
 int BinarySearch(std::vector<int>& MainChain, int loser, int winner) {
 }
 
+int SortNumbers(std::vector<Node>& pairs) {
+  for (int i = 0; i < pairs.size(); i++) {
+    if ()
+  }
+}
+
 void VectorTest(std::vector<Node>& pairs) {
   std::vector<int> MainChain;
   std::vector<int> PendingElements;
+  std::vector<Node> RecPairs;
 
-  
 }
