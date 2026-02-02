9555a51eb745fd5fe09b2aa6f19ba1379ec2515e
Abdellahsyani <syaniabdos@gmail.com>
2026-02-02 11:05:05 +0100

    ford-johnson is hard than what you think..

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -7,13 +7,26 @@ int BinarySearch(std::vector<int>& MainChain, int loser, int winner) {
 std::vector<Node> SortNumbers(std::vector<Node>& pairs) {
   if (pairs.size() <= 1)
     return pairs;
-  for (int i = 0; i < pairs.size(); i += 2) {
-    if (pairs[i].first > pairs[i].)
+  for (size_t i = 0; i < pairs.size(); i += 2) {
+    if (pairs[i].first > pairs[i].losers[i])
   }
   return SortNumbers();
 }
 
+std::vector<Node> sortRecursion(std::vector<Node>& pairs) {
+  if (pairs.size() <= 1) {
+    return pairs;
+  }
+
+  std::vector<Node> nextRound = makePair(pairs);
+  std::vector<Node> mainChain = sortRecursion(nextRound);
+
+  std::vector<Node> result = SortNumbers(mainChain);
+  return result;
+}
+
 void VectorTest(std::vector<Node>& pairs) {
-  std::vector<int> MainChain;
-  std::vector<int> PendingElements;
+  if (pairs.size() <= 1)
+      return pairs;
+
 }
